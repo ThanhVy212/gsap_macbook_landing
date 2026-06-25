9cbaa8d5e2c8652f3173acd77d578a16fff0c5cc
ThanhVy212 <buivy22122004@gmail.com>
2026-06-25 23:18:38 +0700

    delete GLTFResult in models

--- src/components/models/Macbook-16.tsx
+++ src/components/models/Macbook-16.tsx
@@ -9,58 +9,10 @@ Title: macbook pro M3 16 inch 2024
 
 import * as THREE from "three";
 import { useGLTF, useTexture } from "@react-three/drei";
-import type { GLTF } from "three-stdlib";
 import type { JSX } from "react/jsx-dev-runtime";
-import type { AnimationAction } from "three";
 import useMacbookStore from "../../store";
 import { useEffect } from "react";
 import { noChangeParts } from "../../constants";
-
-type GLTFResult = GLTF & {
-  nodes: {
-    Object_10: THREE.Mesh;
-    Object_16: THREE.Mesh;
-    Object_20: THREE.Mesh;
-    Object_22: THREE.Mesh;
-    Object_30: THREE.Mesh;
-    Object_32: THREE.Mesh;
-    Object_34: THREE.Mesh;
-    Object_38: THREE.Mesh;
-    Object_42: THREE.Mesh;
-    Object_48: THREE.Mesh;
-    Object_54: THREE.Mesh;
-    Object_58: THREE.Mesh;
-    Object_66: THREE.Mesh;
-    Object_74: THREE.Mesh;
-    Object_82: THREE.Mesh;
-    Object_96: THREE.Mesh;
-    Object_107: THREE.Mesh;
-    Object_123: THREE.Mesh;
-    Object_127: THREE.Mesh;
-  };
-  materials: {
-    PaletteMaterial001: THREE.MeshStandardMaterial;
-    zhGRTuGrQoJflBD: THREE.MeshStandardMaterial;
-    PaletteMaterial002: THREE.MeshStandardMaterial;
-    lmWQsEjxpsebDlK: THREE.MeshStandardMaterial;
-    LtEafgAVRolQqRw: THREE.MeshStandardMaterial;
-    iyDJFXmHelnMTbD: THREE.MeshStandardMaterial;
-    eJObPwhgFzvfaoZ: THREE.MeshStandardMaterial;
-    nDsMUuDKliqGFdU: THREE.MeshStandardMaterial;
-    CRQixVLpahJzhJc: THREE.MeshStandardMaterial;
-    YYwBgwvcyZVOOAA: THREE.MeshStandardMaterial;
-    SLGkCohDDelqXBu: THREE.MeshStandardMaterial;
-    WnHKXHhScfUbJQi: THREE.MeshStandardMaterial;
-    fNHiBfcxHUJCahl: THREE.MeshStandardMaterial;
-    LpqXZqhaGCeSzdu: THREE.MeshStandardMaterial;
-    gMtYExgrEUqPfln: THREE.MeshStandardMaterial;
-    PaletteMaterial003: THREE.MeshStandardMaterial;
-    JvMFZolVCdpPqjj: THREE.MeshStandardMaterial;
-    sfCQkHOWyrsLmor: THREE.MeshStandardMaterial;
-    ZCDwChwkbBfITSW: THREE.MeshStandardMaterial;
-  };
-  animations: AnimationAction[];
-};
 
 export default function MacbookModel16(props: JSX.IntrinsicElements["group"]) {
   const { color } = useMacbookStore();
