899c5036738a7f8feed9a31dcbdb71dffe7b9116
ThanhVy212 <buivy22122004@gmail.com>
2026-06-25 23:07:03 +0700

    implement features & footer sections

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,28 @@
+import { footerLinks } from "../constants";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="info">
+        More ways to shop: Find an Apple Store or other retailernear you. Or
+        call 000800 040 1966.
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+
+      <hr />
+      <div className="links">
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
